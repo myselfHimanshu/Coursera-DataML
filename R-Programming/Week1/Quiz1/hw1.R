1a34c144e6d908d9ed968bc2442f186b2213d88e
Himanshu Panwar <namecode02@yahoo.in>
2015-08-09 16:08:24 +0530

    Update and rename hw1.html to hw1.R

---
+++ R-Programming/Week1/Quiz1/hw1.R
@@ -0,0 +1,40 @@
+#get the dataset
+#read the csv file
+data = read.csv("/run/media/Himanshu/2B51E07B186F2598/Coursera/RprogrammingLang/hw1_data.csv")
+
+#get the columns names from the dataset
+names(data)
+#first two rows
+head(data,2)
+#last two rows
+tail(data,2)
+
+#observations (i.e. rows) are in this data frame
+nrow(data)
+
+#value of Ozone in the 47th row
+data[[47,1]]
+
+#count number of missing values in Ozone column
+missing <- subset(data,is.na(Ozone))
+nrow(missing)
+
+#get the mean of the column Ozone
+osub <- subset(data,!is.na(Ozone),select = Ozone)
+apply(osub, 2, mean)
+
+
+#mean of colum Solar.R given condition
+sosub <- subset(data,Ozone>31 & Temp>90,select = Solar.R)
+apply(sosub,2,mean)
+
+#mean of Temp given condition
+tempsub <- subset(data,Month==6,select = Temp)
+apply(tempsub,2,mean)
+
+#maximum element in Ozone column given condition
+ozone_max_sub <- subset(data,Month==5 & !is.na(Ozone),select = Ozone)
+apply(ozone_max_sub,2,max)
+
+
+
